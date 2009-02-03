a04e05721674aaecdf22831dc2ecc1d7dcee866d
Adam Robbie <adam.robbie@gmail.com>
2009-02-02 20:55:52 -0500

    n

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Bookins
+//
+//  Created by Adam Robbie on 11/7/08.
+//  Copyright __MyCompanyName__ 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
